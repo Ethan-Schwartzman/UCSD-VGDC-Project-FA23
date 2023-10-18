89cc1a36761ca800935801d06f18b82512e8a62b
Ethan Schwartzman <ethancschwartzman@gmail.com>
2023-10-18 15:06:36 -0700

    Audio Manager
    simple setup for playing sound effects

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] Sounds;
+    
+    public void PlaySound(String name) {
+        // Loop through every sound
+        foreach(Sound s in Sounds) {
+            // Play the sound with the matching name
+            if(s.soundName.Equals(name)) {
+                AudioClip clip = s.sound;
+                AudioSource.PlayClipAtPoint(clip, new Vector3(), s.volume);
+                return;
+            }
+        }
+        Debug.Log("No sound with name \"" + name + "\" was found");
+    }
+}
---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound : MonoBehaviour
+{
+    public AudioClip sound;
+    public String soundName;
+    public float volume;
+}

fbb16bb506dc56667e90d559077dd6d495786493
RadioMediocre <qiwen108@gmail.com>
2023-10-21 16:42:20 -0700

    Monster Timer Control Update
    Added check for monster type
    
    Changed how time is displayed in timer

--- Assets/Scripts/MonsterTypeManager.cs
+++ Assets/Scripts/MonsterTypeManager.cs
@@ -2,16 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MonsterTypeManager : MonoBehaviour
-{
+public class MonsterTypeManager : MonoBehaviour {
     public MonsterType[] MonsterTypes;
-    
+
     public static MonsterType currentMonster;
 
     void Start() {
         currentMonster = ChooseRandomMonsterType();
+        Debug.Log(currentMonster);
     }
     private MonsterType ChooseRandomMonsterType() {
         return MonsterTypes[Random.Range(0, MonsterTypes.Length)];
+    }
+    public MonsterType getCurrentMonster() {
+        return currentMonster;
     }
 }
